defbcac638538a6c35be17ff17b0a3fd456849ce
Dave Bakshani <dave@bakshani.net>
2020-09-29 21:38:25 +0000

    Add second tag

--- main.tf
+++ main.tf
@@ -124,7 +124,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "devops"
+    Department = "devops",
+    Billable = "true"
   }
 }
 
